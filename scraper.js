63d21a390ff2ddf86545d2dd17a85422c0956cdc
tjcichra <tjcichra@yahoo.com>
2020-06-26 16:35:39 -0400

    Working version

--- scraper.js
+++ scraper.js
@@ -29,24 +29,34 @@ con.connect(function(err) {
 
 			const textContent = await page.evaluate(() => {
 				const tds = Array.from(document.querySelectorAll(".x-grid3-body")[1].childNodes)
-				return tds.map(c => c.childNodes)
+				return tds.map(c => c.childNodes).map(c => c[0]).map(c => c.childNodes[0]).map(function(c) {
+					var data = []
+					var i
+
+					for(i = 1; i < 13; i++) {
+						data.push(c.rows[0].cells[i].textContent)
+					}
+					return data
+				})
 			});
 
-			console.log(textContent)
+			//console.log(textContent)
 
 			var i;
+			var j;
 			for(i = 0; i < textContent.length; i++) {
-				var parsedstring = textContent[i].split(/[a-z]|[A-Z]|[ :\-%#]/)
-				var filtered = parsedstring.filter(function (el) {
-					return el != "";
-				});
-				console.log(filtered)
-				if(filtered.length == 5) {
-					con.query("insert into info (frequency, snr, signal_strength) values (" + filtered[2] + ", 0, " + filtered[4] + ");", function(err, result) {
+				for(j = 0; j < textContent[i].length; j++) {
+					textContent[i][j] = textContent[i][j].replace(/[a-z]|[\s:\-%#]/gi, '')
+				}
+
+				if(textContent[i][0] != '' && textContent[i][10] != '' && textContent[i][11] != '') {
+					con.query("insert into info (frequency, snr, signal_strength) values (" + textContent[i][1] + "," + textContent[i][10] + "," + textContent[i][11] + ");", function(err, result) {
 						if(err) throw err;
 					});
 				}
 			}
+
+			await page.waitFor(5000)
 		}
 
 		browser.close()
